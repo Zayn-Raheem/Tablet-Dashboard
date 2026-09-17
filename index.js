3aa338432b5a95e0f4b24707d3b88c2f1c89315d
Zayn Raheem` <Z.raheem114@gmail.com>
2026-09-17 23:44:00 +0100

    added api weather system

--- index.js
+++ index.js
@@ -25,6 +25,36 @@ function updateDate() {
     console.log(dateToday);
 }
 
+async function updateWeather() {
+    const weatherApi = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Birmingham,UK&appid=93eab7c744e431f9d47792b7b9f092388&units=metric')
+    const weatherData = await weatherApi.json()
+    const temperature = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;    
+    const humidity = weatherData.main.humidity;
+    const humidityDisplay = `HUMIDITY: ${humidity}%`
+
+    const windSpeed = weatherData.wind.speed;
+    const windSpeedKmh = Math.round(windSpeed * 3.6);
+    const windDisplay = `WIND: ${windSpeedKmh}KM/H`
+
+    const cloudCover = weatherData.clouds.all;
+    const cloudCoverDisplay = `Cloud Cover: ${cloudCover}%`
+
+    const feelsLike = weatherData.main.feels_like;
+    const feelsLikeDisplay = `Feels like: ${feelsLike}°C` 
+    
+
+    document.getElementById("temperature").innerHTML= temperature;
+    document.getElementById("weather").innerHTML= weatherDescription;
+    document.getElementById("humidity").innerHTML = humidityDisplay;
+    document.getElementById("windSpeed").innerHTML = windDisplay;
+    document.getElementById("cloud_cover").innerHTML = cloudCoverDisplay;
+    document.getElementById("feels_like").innerHTML = feelsLikeDisplay;
+
+
+}
+
 setInterval(updateClock, 1000);
 updateClock();
+updateWeather();
 updateDate();
